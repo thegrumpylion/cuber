f27b217480695f2093dba300d3f70fee1c37dbbe
Nikolas Sepos <nikolas.sepos@gmail.com>
2024-02-07 20:20:13 +0200

    cube component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // App.tsx
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useBluetooth } from './useBluetooth';
-import * as SRVisualizer from 'sr-visualizer';
 import { useTimer } from './useTimer'
 import { StateMachine, states } from './statemachine';
+import Cube from './Cube';
 
 const App = () => {
 
@@ -15,30 +15,22 @@ const App = () => {
 
 
   useEffect(() => {
-    const imgContainer = document.querySelector('#imageContainer') as HTMLDivElement;
-    if (imgContainer) {
-      imgContainer.innerHTML = '';
-      if (stateMachine.process(cubeState)) {
-        if (stateMachine.isComplete()) {
-          console.log('Cube is complete');
-          stop()
-          stateMachine.reset()
-          return
-        }
-        console.log('entering state:', stateMachine.getCurrentStateName());
-        recordEvent(stateMachine.getCurrentStateName())
+    if (stateMachine.process(cubeState)) {
+      if (stateMachine.isComplete()) {
+        console.log('Cube is complete');
+        stop()
+        stateMachine.reset()
+        return
       }
-      SRVisualizer.cubeSVG(
-        imgContainer,
-        `visualcube.php?fmt=svg&r=x-90y-120x-20&size=300&fc=${cubeState}`
-      );
+      console.log('entering state:', stateMachine.getCurrentStateName());
+      recordEvent(stateMachine.getCurrentStateName())
     }
-  }, [cubeState, stateMachine])
+  }, [stateMachine, cubeState, recordEvent, stop])
 
   return (
     <div className="App">
       <div>
-        <div id="imageContainer"></div>
+        <Cube state={cubeState} />
         <button onClick={connect}>Connect</button>
       </div>
       <div>
